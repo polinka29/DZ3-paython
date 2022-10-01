7e070e34da989bd4fa36b18509e3c79b46d42ba8
Polina <polinka29enp@gmail.com>
2022-10-01 07:43:16 +0300

    Zadacha2

---
+++ Zadacha2.py
@@ -0,0 +1,15 @@
+# Напишите программу, которая найдёт произведение пар чисел списка.
+#  Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Пример:
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+
+from random import randint
+num = []
+for i in range(randint(3, 5)):
+    num.append(randint(2, 7))
+res = []
+for i in range((len(num) // 2)):
+    res.append(num[i] * num[len(num) - 1 - i])
+print(num, res, sep=' -> ')
